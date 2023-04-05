bf82212d068f291e7e896e47478ea8dd4724d80c
Vincentx15 <vincent.mallet96@gmail.com>
2023-04-05 18:54:55 +0200

    avoid using batchnorm on too small inputs

--- model.py
+++ model.py
@@ -10,23 +10,27 @@ class ConvBlock(nn.Module):
     Perform 3D conv, BN and elu activation to go from in_channels to out_channels
     """
 
-    def __init__(self, in_channels, out_channels, k_size=3, stride=1, padding=1):
+    def __init__(self, in_channels, out_channels, use_batch_norm=True, k_size=3, stride=1, padding=1):
         super(ConvBlock, self).__init__()
         midchann = int((in_channels + out_channels) / 2)
 
         self.conv3d_a = nn.Conv3d(in_channels=in_channels, out_channels=midchann, kernel_size=k_size,
                                   stride=stride, padding=padding)
-        self.batch_norm_a = nn.BatchNorm3d(num_features=midchann)
         self.conv3d_b = nn.Conv3d(in_channels=midchann, out_channels=out_channels, kernel_size=k_size,
                                   stride=stride, padding=padding)
-        self.batch_norm_b = nn.BatchNorm3d(num_features=out_channels)
+        self.use_batch_norm = use_batch_norm
+        if use_batch_norm:
+            self.batch_norm_a = nn.BatchNorm3d(num_features=midchann)
+            self.batch_norm_b = nn.BatchNorm3d(num_features=out_channels)
 
     def forward(self, x):
         x = self.conv3d_a(x)
-        x = self.batch_norm_a(x)
+        if self.use_batch_norm:
+            x = self.batch_norm_a(x)
         x = F.elu(x)
         x = self.conv3d_b(x)
-        x = self.batch_norm_b(x)
+        if self.use_batch_norm:
+            x = self.batch_norm_b(x)
         x = F.elu(x)
         return x
 
@@ -39,8 +43,8 @@ class RMSDModel(nn.Module):
     def __init__(self, channels=(5, 128, 256, 512, 512)):
         super(RMSDModel, self).__init__()
         self.convs = nn.ModuleList()
-        for prev, next in zip(channels, channels[1:]):
-            self.convs.append(ConvBlock(prev, next))
+        for i, (prev, next) in enumerate(zip(channels, channels[1:])):
+            self.convs.append(ConvBlock(prev, next, use_batch_norm=i < 3))
             self.convs.append(nn.MaxPool3d(kernel_size=3, stride=2))
 
         # self.fc = nn.ModuleList()
